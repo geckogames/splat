7cbf4335222ec14da10b2d5579b75bd2fa7d5f54
htmlguy <htmlguy999@gmail.com>
2015-02-01 16:06:05 -0500

    Add pepper slime

--- levels.js
+++ levels.js
@@ -6,7 +6,8 @@ var level_basic = function () {
         new Player(0, 500 - 128),
         new SlimeBasic(128, 500 - 128),
         new SlimeLearning(256, 500 - 128),
-        new SlimeMutant(400, 500 - 128)
+        new SlimeMutant(400, 500 - 128),
+        new SlimePepper(300, 500 - 128)
     ];
     this.floor = [];
     for(var i = 0; i < 500; i++) {
