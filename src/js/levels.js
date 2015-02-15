21fbd5216852739ccef184d12c2dd2de41e6031b
1dharmadav <davidadharma@gmail.com>
2015-02-14 23:27:19 -0500

    idk

---
+++ src/js/levels.js
@@ -0,0 +1,25 @@
+// SPLAT LEVELS
+var level_basic = function () {
+    // Basic Level
+    this.name = "LEVEL_BASIC";
+    this.objects = [
+        new Player(0, 500 - 128),
+        new SlimeBasic(128, 500 - 128),
+        new SlimeLearning(256, 500 - 128),
+        new SlimeMutant(400, 500 - 128),
+        new SlimePepper(300, 500 - 128)
+<<<<<<< HEAD:levels.js
+        new SlimeHutt(64, 500 - 128)
+        new Bush(64, 500 - 128)
+        new Bush2(175, 500 - 128)
+        new Bush(450, 500 - 128)
+        new Tree(430, 500 - 128)
+    ];
+=======
+    ]
+>>>>>>> origin/master:src/js/levels.js
+    this.floor = [];
+    for(var i = 0; i < 500; i++) {
+        this.floor.push(i / 2)
+    }
+}
