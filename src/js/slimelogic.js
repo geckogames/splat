0258137d24df6ba5ff99bb71712b32ba7076b8ca
htmlguy <htmlguy999@gmail.com>
2015-03-06 13:59:10 -0500

    Slime Back-And-Forth

--- src/js/slimelogic.js
+++ src/js/slimelogic.js
@@ -9,10 +9,12 @@ var slimelogic = {
             x: slimex
         };
     },
-    path_movement: function (cur_ind, max) {
+    path_movement: function (cur_ind, max, speed, x) {
         // calculate difference movement on a back-and-forth path
         // cur_ind maximum is double 'max'
         // max is the maximum x
-        return ; // Return new relative location
+        var max_dir = max / 2; // Max for either directory
+        var direction = (cur_ind > max_dir) ? -1 : 1;
+        return { new_index: (cur_ind + 1) % max, new_x: x + direction * speed }; // Return new relative location
     }
 };
