7a9ee15d28310a4051c8df107ea3d4feeda5a32c
1dharmadav <davidadharma@gmail.com>
2015-03-06 13:42:02 -0500

    He will never be the champion high jumper.

--- src/js/obj/player.js
+++ src/js/obj/player.js
@@ -15,8 +15,8 @@ var Player = function (x, y) {
     this.velocity_y = 0
     this.velocity_x = 0
     this.friction = 0.8
-    this.gravity = 0.5 // Gravitational constant for SplatLand's People
-    this.jump_released = true // Wether or not jump key has been released since last jump
+    this.gravity = 0.6 // Gravitational constant for SplatLand's People
+    this.jump_released = true // Whether or not jump key has been released since last jump
     this.n_jumps = 0 // Number of jumps executed without hitting the ground
     this.update = function (goe, ticks) {
         if(iskeydown(keybindings.RIGHT)) { // Move Right
@@ -34,7 +34,7 @@ var Player = function (x, y) {
         if(Math.floor(this.velocity_x) === 0) this.velocity_x = 0;
         if(iskeydown(keybindings.JUMP) && this.jump_released && this.n_jumps < 2) { // If liable to jump (max double-jump)
             // Set to jump velocity
-            this.velocity_y = -12
+            this.velocity_y = -10
             // Record that the jump button was not released
             this.jump_released = false
             // Increment Number of Jumps (Max double-jump)
