e4261e9c62c20a0470d79b126947fcc6cc11e41a
1dharmadav <davidadharma@gmail.com>
2015-03-18 22:19:21 -0400

    variety! learning slime looks smart and is supposed to be smart.

--- src/js/obj/learningslime.js
+++ src/js/obj/learningslime.js
@@ -18,11 +18,11 @@ var SlimeLearning = function (x, y) {
     this.SPEED = 1
     this.current_path_index = 0;
     this.update = function () {
-        //var pos = slimelogic.movetowardsplayer(game.gos.objects[0].x, this.x, this.truex, this.SPEED)
-        //this.x = pos.x
-        //this.truex = pos.truex
-        var pm = slimelogic.path_movement(this.current_path_index, 300, 1, this.x);
-        this.current_path_index = pm.new_index;
-        this.x = pm.new_x;
+        var pos = slimelogic.movetowardsplayer(game.gos.objects[0].x, this.x, this.truex, this.SPEED)
+        this.x = pos.x
+        this.truex = pos.truex
+        //var pm = slimelogic.path_movement(this.current_path_index, 300, 1, this.x);
+        //this.current_path_index = pm.new_index;
+        //this.x = pm.new_x;
     }
 }
