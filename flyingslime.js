de06a1849fec7c3c9af1cfa0e0f6e5f42ab08d55
1dharmadav <davidadharma@gmail.com>
2015-03-19 22:39:55 -0400

    flappy slime

---
+++ flyingslime.js
@@ -0,0 +1,31 @@
+/*
+    Splat! Pepper Slime JavaScript Script
+    Copyright (C) 2015 GeckoGames
+    All Rights Reserved
+
+    And, we hope you have fun!
+*/
+var SlimeFlying = function (x, y) {
+    this.name = "FLYING_SLIME_OBJECT"
+    this.x = x
+    this.y = y
+    this.truex = x
+    this.image = document.querySelector("#flying_slime")
+    this.w = 128
+    this.h = 128
+    this.enemy = true
+    this.solid = true
+    this.SPEED = 3
+    this.current_path_index = 0;
+    this.update = function () {
+        //var pos = slimelogic.movetowardsplayer(game.gos.objects[0].x, this.x, this.truex, this.SPEED)
+        //this.x = pos.x
+        //this.truex = pos.truex
+        //var pm = slimelogic.path_movement(this.current_path_index, 300, 2, this.x);
+        //this.current_path_index = pm.new_index;
+        //this.x = pm.new_x;
+		var pos = slimelogic.dash(game.gos.objects[0].x, this.x, this.truex, this.SPEED)
+        this.x = pos.x
+        this.truex = pos.truex
+    }
+}
