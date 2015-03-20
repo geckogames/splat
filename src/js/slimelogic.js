a4845045af27726fb5c914d4fae22a880f9d76df
1dharmadav <davidadharma@gmail.com>
2015-03-19 22:41:25 -0400

    dash is meant to be so that it sees the player and goes forward, never stopping. (put it at the beginning of a level and let the player run!)

--- src/js/slimelogic.js
+++ src/js/slimelogic.js
@@ -16,5 +16,14 @@ var slimelogic = {
         var max_dir = max / 2; // Max for either directory
         var direction = (cur_ind > max_dir) ? -1 : 1;
         return { new_index: (cur_ind + 1) % max, new_x: x + direction * speed }; // Return new relative location
-    }
+    },
+	dash: function (playerx, slimex, realx, SPEED) {
+		 if(slimex != playerx) {
+            realx += (playerx > slimex) ? SPEED : -SPEED;
+            slimex = Math.floor(realx);
+        }
+        return {
+            truex: realx,
+            x: slimex
+	}
 };
