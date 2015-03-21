ef3e199da2a3b28a206eb9b92866dd999648fed1
1dharmadav <davidadharma@gmail.com>
2015-03-21 13:44:10 -0400

    dropin

--- src/js/levels.js
+++ src/js/levels.js
@@ -3,19 +3,17 @@ var level_basic = function () {
     // Basic Level
     this.name = "LEVEL_BASIC";
     this.objects = [
-        new Player(0, 500 - 128),
+        new Player(0, 500 - 600),
         new SlimeBasic(300, 500 - 128),
         new SlimeLearning(400, 500 - 128),
         new SlimeBasic(800, 500 - 128),
         new SlimeBasic(600, 500 - 128),
 		new SlimeBasic(1000, 500 - 128),
 		new SlimeBasic(1300, 500 - 128),
-		new Bush(64, 500 - 128),
-        new Bush2(175, 500 - 128),
-        new Bush(450, 500 - 128),
-        new Tree(430, 500 - 128),
-		new Bush(1400, 500 - 128),
-		new Tree2(1200, 500 - 128)
+        new Bush(400, 500 - 400),
+        new Tree(430, 500 - 400),
+		new Bush2(1400, 500 - 400),
+		new Tree2(1200, 500 - 400)
     ];
     this.floor = [];
     for(var i = 0; i < 500; i++) {
