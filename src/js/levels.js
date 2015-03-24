2492c19875d1418cb32054cab5b762208635ba57
1dharmadav <davidadharma@gmail.com>
2015-03-23 20:28:24 -0400

    Level 2!!!!! radioactiveness!! YAY!!!!

--- src/js/levels.js
+++ src/js/levels.js
@@ -21,3 +21,25 @@ var level_basic = function () {
         this.floor.push(i / 2)
     }
 }
+var level_2 = function () {
+    //Level 2
+    this.name = "LEVEL_2";
+    this.objects = [
+        new Player(0, 500 - 600),
+        new SlimeMutant(300, 500 - 128),
+        new SlimeLearning(400, 500 - 128),
+        new SlimeMutant(800, 500 - 128),
+        new SlimeMutant(600, 500 - 128),
+		new SlimeMutant(1000, 500 - 128),
+		new SlimeMutant(1300, 500 - 128),
+        new Bush2(400, 500 - 400),
+		new Bush2(50, 650 - 400),
+        new Bush2(430, 500 - 400),
+		new Bush2(1400, 500 - 400),
+		new Bush2(1200, 500 - 400)
+    ];
+    this.floor = [];
+    for(var i = 0; i < 500; i++) {
+        this.floor.push(i / 2)
+    }
+}
