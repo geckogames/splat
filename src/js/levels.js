97319f462a73a45b2f407340958aa8c4c170b46e
htmlguy <htmlguy999@gmail.com>
2015-03-24 16:56:51 -0400

    Clean up after your 1dharmadav

--- src/js/levels.js
+++ src/js/levels.js
@@ -4,28 +4,29 @@ var level_basic = function () {
     this.name = "LEVEL_BASIC";
     this.objects = [
         new Player(0, 500 - 600),
+        new Bush(400, 500 - 128),
+		new Bush2(50, 650 - 128),
+        new Tree(430, 500 - 256),
+		new Bush(1400, 500 - 128),
+		new Tree2(1200, 500 - 256),
         new SlimeBasic(300, 500 - 128),
         new SlimeLearning(400, 500 - 128),
         new SlimeBasic(800, 500 - 128),
         new SlimeBasic(600, 500 - 128),
 		new SlimeBasic(1000, 500 - 128),
-		new SlimeBasic(1300, 500 - 128),
-        new Bush(400, 500 - 400),
-		new Bush2(50, 650 - 400),
-        new Tree(430, 500 - 400),
-		new Bush(1400, 500 - 400),
-		new Tree2(1200, 500 - 400)
+		new SlimeBasic(1300, 500 - 128)
     ];
-    this.floor = [];
-    for(var i = 0; i < 500; i++) {
-        this.floor.push(i / 2)
-    }
 }
 var level_2 = function () {
     //Level 2
     this.name = "LEVEL_2";
     this.objects = [
         new Player(0, 500 - 600),
+        new Bush2(400, 500 - 400),
+		new Bush2(50, 650 - 400),
+        new Bush2(430, 500 - 400),
+		new Bush2(1400, 500 - 400),
+		new Bush2(1200, 500 - 400),
         new SlimeMutant(300, 500 - 128),
         new SlimeLearning(400, 500 - 128),
         new SlimeMutant(800, 500 - 128),
@@ -33,15 +34,6 @@ var level_2 = function () {
 		new SlimeFlying(1150, 500 - 128),
         new SlimeMutant(600, 500 - 128),
 		new SlimeMutant(1000, 500 - 128),
-		new SlimeMutant(1300, 500 - 128),
-        new Bush2(400, 500 - 400),
-		new Bush2(50, 650 - 400),
-        new Bush2(430, 500 - 400),
-		new Bush2(1400, 500 - 400),
-		new Bush2(1200, 500 - 400)
+		new SlimeMutant(1300, 500 - 128)
     ];
-    this.floor = [];
-    for(var i = 0; i < 500; i++) {
-        this.floor.push(i / 2)
-    }
 }
