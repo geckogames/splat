1b7e169d9ffc16dcbc5e4a7837fc8069738cbb20
htmlguy <htmlguy999@gmail.com>
2015-03-24 17:30:57 -0400

    Flying Slime Woot Woot

--- src/js/levels.js
+++ src/js/levels.js
@@ -28,16 +28,16 @@ var level_2 = function () {
     this.name = "LEVEL_2";
     this.objects = [
         new Player(0, 500 - 600),
-        new Bush2(400, 500 - 400),
-		new Bush2(50, 650 - 400),
-        new Bush2(430, 500 - 400),
-		new Bush2(1400, 500 - 400),
-		new Bush2(1200, 500 - 400),
+        new Bush2(400, 500 - 128),
+		new Bush2(50, 650 - 128),
+        new Bush2(430, 500 - 128),
+		new Bush2(1400, 500 - 128),
+		new Bush2(1200, 500 - 128),
         new SlimeMutant(300, 500 - 128),
         new SlimeLearning(400, 500 - 128),
         new SlimeMutant(800, 500 - 128),
-		//new SlimeFlying(400, 500 - 128),
-		//new SlimeFlying(1150, 500 - 128),
+		new SlimeFlying(400, 100),
+		new SlimeFlying(1150, 200),
         new SlimeMutant(600, 500 - 128),
 		new SlimeMutant(1000, 500 - 128),
 		new SlimeMutant(1300, 500 - 128)
