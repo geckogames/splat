2fb5d1cd1a27fdbccb73f70c12bfafba44c78dd6
1dharmadav <davidadharma@gmail.com>
2015-03-30 19:01:40 -0400

    zdsfxgch

--- src/js/obj/flyingslime.js
+++ src/js/obj/flyingslime.js
@@ -15,14 +15,14 @@ var SlimeFlying = function (x, y) {
     this.h = 128
     this.enemy = true
     this.solid = true
-    this.SPEED = 1
-    this.current_path_index = 0;
+    this.SPEED = 5
+    this.current_path_index = 5;
     this.update = function () {
-        var pos = slimelogic.movetowardsplayer(game.gos.objects[0].x, this.x, this.truex, this.SPEED)
-        this.x = pos.x
-        this.truex = pos.truex
-        //var pm = slimelogic.path_movement(this.current_path_index, 300, 1, this.x);
-        //this.current_path_index = pm.new_index;
-        //this.x = pm.new_x;
+        //var pos = slimelogic.movetowardsplayer(game.gos.objects[0].x, this.x, this.truex, this.SPEED)
+        //this.x = pos.x
+        //this.truex = pos.truex
+        var pm = slimelogic.path_movement(this.current_path_index, 300, 1, this.x);
+        this.current_path_index = pm.new_index;
+        this.x = pm.new_x;
     }
 }
