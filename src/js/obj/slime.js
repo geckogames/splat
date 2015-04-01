ff84d5d0e3bd31ecae8e4a555f95496e87577228
htmlguy <htmlguy999@gmail.com>
2015-04-01 11:50:14 -0400

    Fix Hitbox for Slimes

--- src/js/obj/slime.js
+++ src/js/obj/slime.js
@@ -25,7 +25,7 @@ var SlimeBasic = function (x, y) {
         this.current_path_index = pm.new_index;
         this.x = pm.new_x;
         var tio = game.gos.objects.indexOf(this);
-        if (iskeydown(keybindings.ATTACK) && game.gos.objects[0].x > game.gos.objects[tio].x && game.gos.objects[0].x < game.gos.objects[tio].x + 128){
+        if (iskeydown(keybindings.ATTACK) && ((game.gos.objects[0].x > game.gos.objects[tio].x && game.gos.objects[0].x < game.gos.objects[tio].x + 128) || (game.gos.objects[0].x + 128 > game.gos.objects[tio].x && game.gos.objects[0].x + 128 < game.gos.objects[tio].x + 128))){
             game.gos.objects.splice(tio, 1);
         }
     }
