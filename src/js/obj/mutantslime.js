5d2a880033395ed1cb3d8ff492afdbb5f0bddfb5
htmlguy <htmlguy999@gmail.com>
2015-04-01 12:02:27 -0400

    Kill all the slimes

--- src/js/obj/mutantslime.js
+++ src/js/obj/mutantslime.js
@@ -24,5 +24,9 @@ var SlimeMutant = function (x, y) {
         var pm = slimelogic.path_movement(this.current_path_index, 350, 3, this.x);
         this.current_path_index = pm.new_index;
         this.x = pm.new_x;
+        var tio = game.gos.objects.indexOf(this);
+        if (iskeydown(keybindings.ATTACK) && ((game.gos.objects[0].x > this.x && game.gos.objects[0].x < this.x + 128) || (game.gos.objects[0].x + 128 > this.x && game.gos.objects[0].x + 128 < this.x + 128)) && game.gos.objects[0].y >= this.y){
+            game.gos.objects.splice(tio, 1);
+        }
     }
 }
