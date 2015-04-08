e2a433f7d3f71028d14b2dbad586f28f4effe390
htmlguy <htmlguy999@gmail.com>
2015-04-08 19:10:24 -0400

    GET GPL'D

--- src/js/obj/swarmslime.js
+++ src/js/obj/swarmslime.js
@@ -1,9 +1,24 @@
 /*
-    Splat! Flying Slime JavaScript Script
+    Splat! Swarm Slime JavaScript Script
     Copyright (C) 2015 GeckoGames
     All Rights Reserved
 
     And, we hope you have fun!
+
+    This file is part of Splat!.
+
+    Splat! is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Splat! is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Splat!.  If not, see <http://www.gnu.org/licenses/>.
 */
 var SlimeSwarm = function (x, y) {
     this.name = "SWARM_SLIME_OBJECT"
